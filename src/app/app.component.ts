2d1fdc5d53de64b9971674eb0f49e43ec49a6a02
Lukas Matta <lukasmatta@users.noreply.github.com>
2024-05-03 13:14:42 +0200

    Prerendering problem

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -6,8 +7,18 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'PrerenderSpaceUrl';
+  prerendered = '';
+  prerendered2 = '';
+
+  constructor(private http: HttpClient) {
+    this.http
+      .get('/assets/Folder%20space/test.txt', { responseType: 'text' })
+      .subscribe((data: any) => (this.prerendered = data));
+    this.http
+      .get('/assets/FolderNospace/test.txt', { responseType: 'text' })
+      .subscribe((data: any) => (this.prerendered2 = data));
+  }
 }
